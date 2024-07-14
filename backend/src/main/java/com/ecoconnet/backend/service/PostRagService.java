cf49e4165b76e4ffcdeb0b3b97fcc1f685cda72d
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2024-07-14 20:50:07 +0530

    histroy backend added

--- backend/src/main/java/com/ecoconnet/backend/service/PostRagService.java
+++ backend/src/main/java/com/ecoconnet/backend/service/PostRagService.java
@@ -1,16 +1,26 @@
 package com.ecoconnet.backend.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ecoconnet.backend.model.PostRagModel;
 import com.ecoconnet.backend.repository.PostRagRepo;
+
+@Service
 public class PostRagService{
     @Autowired
     PostRagRepo repo;
 
     
-    PostRagModel postragservice(PostRagModel p)
+    public PostRagModel postragservice(PostRagModel p)
     {
         return repo.save(p);
+    }
+
+    public List<PostRagModel> getRags() {
+        
+        return repo.findAll();
     }
 }
