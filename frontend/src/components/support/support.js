eac5470da26e69ef6fcf798a987ad3b2ca93d007
Parthiban-P-dev <727722euit125@skcet.ac.in>
2024-07-14 12:47:43 +0530

    change in support page

--- frontend/src/components/support/support.js
+++ frontend/src/components/support/support.js
@@ -11,25 +11,25 @@ const Data =[
         id:1,
         img:customer,
         title:'CUSTOMER SUPPORT',
-        desc:"Click here to read our FAQs For support with your bookings and other queries, email us at help@porter.inor call us on022 4410 4410"
+        desc:"For support with your bookings and other queries, email us at help@ecoconnect.ac.in inor call us on022 4410 4410"
     },
     {
         id:2,
         img:drive,
-        title:'PACKERS AND MOVERS',
-        desc:"For queries and support regarding your house shifting booking, email us atpackermover@theporter.inor call us on022 4410 4444or022 6268 4444"
+        title:'RAG PICKER',
+        desc:"For queries and support regarding your house shifting booking, email us at pickersupport@ecoconnect.ac.in inor call us on022 4410 4444or022 6268 4444"
     },
     {
         id:3,
         img:enter,
         title:'ENTERPRISE SERVICES',
-        desc:"If you are an enterprise client and need trucks in bulk for your business,Click hereor mail us athelp@porter.in"
+        desc:"If you are an enterprise client and have pulk rags,Click hereor mail us help@ecoconnect.ac.in."
     },
     {
         id:4,
         img:lorry,
-        title:'DRIVE WITH PORTER',
-        desc:"Are you a tempo truck owner? Increase your earnings by partnering with us. Click hereor reach us out on022 4410 4410(add your city code)"
+        title:'DRIVE WITH ECOCONNECT',
+        desc:"Are you a rag picker? Increase your earnings by partnering with us. Click here or reach us out on 022 4410 4410(add your city code)"
     },
 ]
 export const Support =()=>{
@@ -61,7 +61,7 @@ export const Support =()=>{
             <div className="office">
                 <h1>Head Office</h1>
                 <h2>Coimbatore</h2>
-                <p>Porter Head Office, BCM Prime 1st Cross,<br /> BDA Layout, Bannerghatta Main Rd, New <br /> Gurappana Palya,Tavarekere Extension,<br />  Coimbatore, TamilNadu 641008</p>
+                <p>Eco Connect Head Office, BCM Prime 1st Cross,<br /> BDA Layout, Ghandhipuram Main Rd, New <br /> Ramanathapuram,Ghandhipuram Extension,<br />  Coimbatore, TamilNadu 641008</p>
                 <a href="https://www.google.com/maps/place/Coimbatore,+Tamil+Nadu/@11.0139578,76.8848334,12z/data=!3m1!4b1!4m6!3m5!1s0x3ba859af2f971cb5:0x2fc1c81e183ed282!8m2!3d11.0168445!4d76.9558321!16zL20vMDE5ZmM0?entry=ttu" target="-blank">
                 Get Direction >
                 </a>
